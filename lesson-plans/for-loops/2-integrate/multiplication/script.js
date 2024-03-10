a4ed01c822c21b4a1a059718972327c933b539bc
Tarun Purswani <tarunpurswani713@gmail.com>
2024-03-10 15:23:36 +0100

    insideJs-Excersice

--- lesson-plans/for-loops/2-integrate/multiplication/script.js
+++ lesson-plans/for-loops/2-integrate/multiplication/script.js
@@ -1,11 +1,19 @@
 import { readNumber, display } from '../../../../lib/dom-io.js';
 
-document.getElementById(__).addEventListener('click', () => {
-  debugger;
-
+document.getElementById('do-math').addEventListener('click', () => {
+ 
   // read user values
+  const num1 = readNumber('left');
+  const num2 = readNumber('right');
 
   // use a for loop to multiply the two numbers
+  let result = 0;
+  for (let i = 0; i < num2; i++) {
+    result += num1;
+  }
 
-  // display the product
+  // display the product  
+ display("product", result);
+
+   debugger;
 });
